5b828d20f732d66e68dd873d3e0bad9e87a019e7
Jonath-z <jonathanzihindula95@gmail.com>
2024-02-10 20:15:06 +0200

    fix: intersection observer initial state

--- src/components/ui/velix/Header.tsx
+++ src/components/ui/velix/Header.tsx
@@ -2,27 +2,35 @@ import React from "react";
 import VelixLogo from "@/components/svg/velix-logo-group.svg";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { useInView } from "react-intersection-observer";
+import Link from "next/link";
 import { Button } from "../button";
 
 export default function Header() {
-  const { ref, inView } = useInView();
+  const { ref, inView } = useInView({
+    threshold: 0.2,
+    initialInView: true
+  });
 
   return (
     <div>
       <header ref={ref} className="flex justify-between items-center py-14">
-        <VelixLogo />
+        <Link href="/">
+          <VelixLogo />
+        </Link>
         <Button className="font-space-grotesk bg-velix-yellow px-10 hover:bg-velix-yellow">
           Launch
         </Button>
       </header>
 
       <header
-        className={`px-5 justify-between -top-full -translate-y-full fixed left-0 right-0 z-20 items-center py-5 transition-all duration-200 bg-velix-primary ${
-          !inView ? "!top-0 !translate-y-0" : ""
+        className={`px-5 justify-between -top-full -translate-y-full fixed left-0 right-0 z-20 items-center py-5 bg-velix-primary ${
+          !inView ? "!top-0 !translate-y-0 transition-all duration-200" : ""
         }`}
       >
         <div className="flex justify-between w-full max-w-5xl xl:max-w-7xl mx-auto relative">
-          <VelixLogo />
+          <Link href="/">
+            <VelixLogo />
+          </Link>
           <Button className="font-space-grotesk bg-velix-yellow px-10 hover:bg-velix-yellow">
             Launch
           </Button>
