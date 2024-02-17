44cf08b0e65fd3eab674b475e1410811cd16a952
Jonath-z <jonathanzihindula95@gmail.com>
2024-02-17 18:28:43 +0200

    feat: add metis sepolia as default net

--- src/lib/wagmi.config.ts
+++ src/lib/wagmi.config.ts
@@ -1,7 +1,7 @@
 import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
-import { mainnet, sepolia, metis } from "wagmi/chains";
 import { cookieStorage, createStorage } from "wagmi";
 import { PROJECT_ID } from "./ constant";
+import { defineChain } from "viem";
 
 // Get projectId at https://cloud.walletconnect.com
 export const projectId = PROJECT_ID;
@@ -9,21 +9,48 @@ export const projectId = PROJECT_ID;
 if (!projectId) throw new Error("Project ID is not defined");
 
 const metadata = {
-  name: "Web3Modal",
-  description: "Web3Modal Example",
-  url: "https://web3modal.com",
+  name: "Velix",
+  description: "Liquidity staking platform",
+  url: "https://velix.io",
   icons: ["https://avatars.githubusercontent.com/u/37784886"]
 };
 
+export const METIS_SEPOLIA = defineChain({
+  id: 59901,
+  name: "METIS sepolia",
+  nativeCurrency: {
+    name: "Metis",
+    symbol: "METIS",
+    decimals: 18
+  },
+  contracts: {
+    multicall3: {
+      address: "0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000"
+    }
+  },
+  blockExplorers: {
+    default: {
+      name: "Metis Sepoliaa Explorer",
+      url: "https://sepolia.explorer.metisdevops.link/"
+    }
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://sepolia.rpc.metisdevops.link/"],
+      webSocket: ["wss://sepolia-ws.rpc.metisdevops.link"]
+    }
+  }
+});
+
 // Create wagmiConfig
 export const config = defaultWagmiConfig({
-  chains: [mainnet, sepolia, metis],
+  chains: [METIS_SEPOLIA],
   projectId,
   metadata,
   storage: createStorage({
     storage: cookieStorage
   }),
-  ssr: true,
+
   enableWalletConnect: true,
   enableInjected: true,
   enableEIP6963: true,
