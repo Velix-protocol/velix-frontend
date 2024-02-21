6b21040a7c1156e2e13d18ad553f1901fb182493
Jonath-z <jonathanzihindula95@gmail.com>
2024-02-21 02:51:29 +0200

    fix: protocol description

--- src/components/section/Stake/index.tsx
+++ src/components/section/Stake/index.tsx
@@ -9,21 +9,21 @@ export default function Stake() {
     <div>
       <Section className="px-5 pb-16 lg:pb-0">
         <div className="grid grid-cols-1 lg:grid-cols-2 lg:grid-rows-2 gap-5 justify-between items-start pt-28 lg:pt-40 pb-20 h-fit">
-          <div className="lg:row-span-2 h-fit">
+          <div className="lg:row-span-2 h-fit mt-[1.3rem]">
             <Title
               name="Stake METIS"
               subtitle="Stake METIS and receive veMETIS"
             />
             <StakingOperations />
           </div>
-          <div className="mt-16 lg:mt-0">
+          <div className="mt-16 lg:mt-0 flex flex-col gap-5">
             <Title
               name="Velix statistics"
               subtitle="View your  veMETIS statistics."
             />
             <Metrics />
+            <Chains />
           </div>
-          <Chains />
         </div>
       </Section>
     </div>
