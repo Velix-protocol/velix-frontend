846cf265ce8ba225b8de51743abf3a7ab0447b1d
Jonath-z <jonathanzihindula95@gmail.com>
2024-02-21 11:31:56 +0200

    chore: change how it works section image

--- src/components/section/HowItWorks/index.tsx
+++ src/components/section/HowItWorks/index.tsx
@@ -7,7 +7,7 @@ export default function HowItWorks() {
     <Section className="py-16 px-5">
       <div className="flex flex-col-reverse  lg:grid lg:grid-cols-2 bg-[#1E1E1E] items-center text-white p-5 lg:p-16 rounded-lg">
         <div>
-          <h2 className="font-space-grotesk font-bold text-2xl lg:text-4xl text-start">
+          <h2 className="font-space-grotesk font-bold text-2xl lg:text-4xl text-center lg:text-start">
             How Velix works
           </h2>
           <Steps />
