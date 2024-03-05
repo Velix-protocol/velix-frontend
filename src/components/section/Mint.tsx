6378df6367be31babc6c20ef819e2349ac1bb5b8
Jonath-z <jonathanzihindula95@gmail.com>
2024-03-05 20:27:57 +0200

    feat: add mint steps modal

--- src/components/section/Mint.tsx
+++ src/components/section/Mint.tsx
@@ -8,10 +8,14 @@ import StakeLayout from "../layouts/StakeLayout";
 import Statitics from "./Statitics";
 import StakeTitleWrapper from "../layouts/StakeTitleWrapper";
 import { ChangeEvent, useState } from "react";
+import Modal from "../ui/velix/Modal";
+import ClockIcon from "../ui/velix/icons/ClockIcon";
 // import { useApproveMinting } from "@/hooks/use-contract";
 
 export default function Mint() {
   const [amountToMint, setAmountToMint] = useState("");
+  const [showModal, setShowModal] = useState(false);
+  const [currentStep] = useState<1 | 2>(1);
   // const  = useApproveMinting()
 
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -21,39 +25,54 @@ export default function Mint() {
 
   const onMint = () => {
     if (!amountToMint || !amountToMint.trim()) return;
+    setShowModal(true);
     console.log("mint");
   };
 
   return (
-    <div>
-      <Section className="px-5 pb-32 lg:pb-16">
-        <StakeTitleWrapper>
-          <div className="w-full">
-            <Title
-              name="Mint"
-              subtitle={
-                <p className="flex items-start justify-start gap-4">
-                  <span>
-                    <InfoIcon className="h-6 w-6 text-white fill-velix-primary" />
-                  </span>
-                  <span>
-                    Mint veMETIS to participate in liquid staking.
-                    <br /> veMETIS is a liquid staking derivative of METIS.
-                  </span>
-                </p>
-              }
-            />
+    <>
+      {showModal && (
+        <Modal isOpened={showModal} onClose={() => setShowModal(false)}>
+          <div className="flex flex-col gap-3 items-center">
+            <ClockIcon className="w-10 h-10 mb-6" />
+            <p className="font-bold text-center text-2xl lg:text-4xl">
+              Waiting for Approval
+            </p>
+            <p className="text-velix-gray text-center text-base">
+              Confirm this transaction in your wallet.
+            </p>
+
+            {currentStep === 2 && (
+              <StakingFormButtom role="mint" onMint={() => null} />
+            )}
+            <div className="flex gap-0 items-center w-fit h-fit mt-8">
+              <p className="text-white bg-velix-primary h-8 w-8 flex justify-center items-center rounded-full">
+                1
+              </p>
+              <div
+                className={`h-1 w-32 ${
+                  currentStep === 1
+                    ? "bg-gradient-to-r from-velix-primary to-velix-gray/20"
+                    : "bg-velix-primary"
+                }`}
+              />
+              <p
+                className={`h-8 w-8 flex justify-center items-center p-2 rounded-full ${
+                  currentStep === 2
+                    ? "text-white bg-velix-primary"
+                    : "bg-velix-gray/20 text-velix-primary"
+                }`}
+              >
+                2
+              </p>
+            </div>
           </div>
-          <div className="w-full">
-            <Title
-              name="Velix statistics"
-              subtitle="View your  veMETIS statistics."
-            />
-          </div>
-        </StakeTitleWrapper>
-        <AppContent>
-          <div className="w-full h-fit">
-            <div className="w-full block lg:hidden mt-32">
+        </Modal>
+      )}
+      <div>
+        <Section className="px-5 pb-32 lg:pb-16">
+          <StakeTitleWrapper>
+            <div className="w-full">
               <Title
                 name="Mint"
                 subtitle={
@@ -69,39 +88,65 @@ export default function Mint() {
                 }
               />
             </div>
-            <StakeLayout
-              onFromValueChange={onChange}
-              showSwapIcon={false}
-              isStaking={false}
-            >
-              <div className="mt-9 flex flex-col gap-7">
-                <StakingDetails
-                  title="Exchange Rate"
-                  value="1 METIS = 1 veMETIS"
-                />
-                <StakingDetails
-                  title="Average return"
-                  value={
-                    <span className="text-xs lg:text-base">
-                      =3.13 <span className="font-bold">APR</span>
-                    </span>
-                  }
-                />
-              </div>
-              <StakingFormButtom onMint={onMint} role="mint" />
-            </StakeLayout>
-          </div>
-          <div className="w-full">
-            <div className="w-full block lg:hidden mt-10">
+            <div className="w-full">
               <Title
                 name="Velix statistics"
                 subtitle="View your  veMETIS statistics."
               />
             </div>
-            <Statitics />
-          </div>
-        </AppContent>
-      </Section>
-    </div>
+          </StakeTitleWrapper>
+          <AppContent>
+            <div className="w-full h-fit">
+              <div className="w-full block lg:hidden mt-32">
+                <Title
+                  name="Mint"
+                  subtitle={
+                    <p className="flex items-start justify-start gap-4">
+                      <span>
+                        <InfoIcon className="h-6 w-6 text-white fill-velix-primary" />
+                      </span>
+                      <span>
+                        Mint veMETIS to participate in liquid staking.
+                        <br /> veMETIS is a liquid staking derivative of METIS.
+                      </span>
+                    </p>
+                  }
+                />
+              </div>
+              <StakeLayout
+                onFromValueChange={onChange}
+                showSwapIcon={false}
+                isStaking={false}
+              >
+                <div className="mt-9 flex flex-col gap-7">
+                  <StakingDetails
+                    title="Exchange Rate"
+                    value="1 METIS = 1 veMETIS"
+                  />
+                  <StakingDetails
+                    title="Average return"
+                    value={
+                      <span className="text-xs lg:text-base">
+                        =3.13 <span className="font-bold">APR</span>
+                      </span>
+                    }
+                  />
+                </div>
+                <StakingFormButtom onMint={onMint} role="mint" />
+              </StakeLayout>
+            </div>
+            <div className="w-full">
+              <div className="w-full block lg:hidden mt-10">
+                <Title
+                  name="Velix statistics"
+                  subtitle="View your  veMETIS statistics."
+                />
+              </div>
+              <Statitics />
+            </div>
+          </AppContent>
+        </Section>
+      </div>
+    </>
   );
 }
---
+++ src/components/ui/velix/Modal.tsx
@@ -0,0 +1,33 @@
+import { ReactNode, useEffect } from "react";
+import CancelIcon from "./icons/CancelIcon";
+
+export default function Modal({
+  children,
+  onClose,
+  isOpened
+}: {
+  children: ReactNode;
+  isOpened: boolean;
+  onClose: () => void;
+}) {
+  useEffect(() => {
+    if (isOpened) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+  }, [isOpened]);
+
+  return (
+    <div className="fixed z-50 px-5 bg-velix-primary/20 inset-0 font-space-grotesk">
+      <div className="bg-white max-w-screen-md mx-auto flex justify-center items-center mt-64 relative p-10 lg:p-20 rounded-lg">
+        <CancelIcon
+          onClick={onClose}
+          role="button"
+          className="w-7 h-7 absolute top-0 right-0 m-5"
+        />
+        {children}
+      </div>
+    </div>
+  );
+}
