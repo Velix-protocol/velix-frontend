794c9cf2cd503da91c6f62c6229393664f6fc465
Jonath-z <jonathanzihindula95@gmail.com>
2024-03-08 02:06:21 +0200

    feat: add stats to the dashboard

--- src/components/section/Dashboard.tsx
+++ src/components/section/Dashboard.tsx
@@ -2,18 +2,54 @@ import { CardContent, Card } from "@/components/ui/DashboardCard";
 import { Button } from "@/components/ui/button";
 import Chart from "./Chart";
 import Section from "../layouts/Section";
+import { useAccount, useBalance } from "wagmi";
+import { useMetisBalance } from "@/hooks/use-contract";
+import { converGweiToEth } from "@/lib/utils";
 
 export default function Dashboard() {
+  const { address } = useAccount();
+  const { data } = useBalance({
+    address
+  });
+
+  const { veMetisbalance, sveMetisBalance } = useMetisBalance(
+    address as string,
+    {
+      operationsDone: true
+    }
+  );
+
+  const velixData = [
+    {
+      name: "METIS",
+      value: converGweiToEth(data?.value ?? BigInt(0))
+    },
+    {
+      name: "veMETIS",
+      value: veMetisbalance
+    },
+    {
+      name: "sveMETIS",
+      value: sveMetisBalance
+    },
+    {
+      name: "APR",
+      value: "20%"
+    }
+  ];
+
   return (
     <Section className="mt-28 px-5 pb-28">
       <div className="py-8 flex-col flex gap-8">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-8 bg-white p-5 lg:p-12 rounded-lg">
-          {Array.from(Array(4).keys()).map((index) => (
+          {velixData.map((data, index) => (
             <Card key={index} className="bg-velix-slate-blue">
               <CardContent className="p-7 space-y-2">
-                <div className="text-sm text-velix-gray">veMETIS balance</div>
+                <div className="text-sm text-velix-gray">
+                  {data.name} balance
+                </div>
                 <div className="text-lg font-semibold text-velix-primary">
-                  0.0 veMETIS
+                  {data.value} {data.name}
                 </div>
               </CardContent>
             </Card>
