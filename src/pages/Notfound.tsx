f9fbc64cd06dd9e947cc9659683757afda30113b
Jonath-z <jonathanzihindula95@gmail.com>
2024-03-18 19:00:02 +0200

    fix: add navigation  to 404 page

--- src/pages/Notfound.tsx
+++ src/pages/Notfound.tsx
@@ -4,9 +4,10 @@ import BottomBar from "@/components/ui/velix/BottomBar";
 import Number from "@/components/ui/velix/icons/404";
 import NotfoundIcon from "@/components/ui/velix/icons/NotfoundIcon";
 import VelixPrimaryBlackLogo from "@/components/ui/velix/icons/VelixPrimaryBlackLogo";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Notfound({ isNested = false }: { isNested?: boolean }) {
+  const navigate = useNavigate();
   return (
     <Section className="px-5 fixed top-0 left-0 right-0 h-screen bg-velix-slate-blue">
       {!isNested && (
@@ -27,7 +28,10 @@ export default function Notfound({ isNested = false }: { isNested?: boolean }) {
         <h2 className="text-2xl md:text-4xl text-center text-velix-primary font-bold font-space-grotesk mb-5 md:mb-10">
           Page not found
         </h2>
-        <Button className="flex bg-white hover:bg-velix-primary text-velix-primary hover:text-white font-space-grotesk px-8 font-bold text-center items-center">
+        <Button
+          onClick={() => navigate("/", { replace: true })}
+          className="flex bg-white hover:bg-velix-primary text-velix-primary hover:text-white font-space-grotesk px-8 font-bold text-center items-center"
+        >
           Go to home
         </Button>
       </div>
