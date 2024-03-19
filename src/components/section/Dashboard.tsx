a6e58d9dea9137d4c8d5f7e11899cd14b803d95b
Jonath-z <jonathanzihindula95@gmail.com>
2024-03-19 22:56:44 +0200

    feat: add metis balances on the dashboard

--- src/components/section/Dashboard.tsx
+++ src/components/section/Dashboard.tsx
@@ -7,9 +7,32 @@ import {
   TableHead,
   TableHeader
 } from "@/components/ui/table";
+import { useBalanceStore } from "@/store/balanceState";
+import { Card, CardContent } from "../ui/DashboardCard";
 
 export default function Dashboard() {
   useMetisBalance();
+  const { sveMETISBalance, veMETISBalance, METISBalance } = useBalanceStore();
+
+  const velixBalances = [
+    {
+      name: "METIS",
+      value: METISBalance
+    },
+    {
+      name: "veMETIS",
+      value: veMETISBalance
+    },
+    {
+      name: "sveMETIS",
+      value: sveMETISBalance
+    },
+    {
+      name: "APR",
+      value: "--"
+    }
+  ];
+
   const velixData = [
     {
       date: "Feb 15 17:04:09",
@@ -50,8 +73,22 @@ export default function Dashboard() {
   ];
 
   return (
-    <Section className="mt-28 px-5 pb-28 min-h-screen">
-      <Table className="p-10 font-space-grotesk mt-16">
+    <Section className="mt-48 px-5 pb-28 min-h-screen">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 bg-white p-5 lg:p-12 rounded-lg">
+        {velixBalances.map((balance, index) => (
+          <Card key={index} className="bg-velix-slate-blue">
+            <CardContent className="p-7 space-y-2">
+              <div className="text-sm text-velix-gray">
+                {balance.name} balance
+              </div>
+              <div className="text-lg font-semibold text-velix-primary">
+                {balance.value} {balance.name}
+              </div>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+      <Table className="p-10 font-space-grotesk mt-10">
         <TableHeader className="bg-velix-primary pb-14 pt-10 rounded-t-xl grid grid-cols-3 justify-between w-full text-white px-8">
           <TableHead className="w-[100px] text-white font-bold h-fit">
             Date
