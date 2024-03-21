ce53e1688a3b42d52bd8d2abf9a837e61f1805bb
Jonath-z <jonathanzihindula95@gmail.com>
2024-03-21 22:07:00 +0200

    style: update the screen height

--- src/components/layouts/Page.tsx
+++ src/components/layouts/Page.tsx
@@ -7,7 +7,7 @@ import WagmiProvider from "@/context/WagmiProvider";
 export default function Page() {
   return (
     <WagmiProvider>
-      <div className="bg-velix-slate-blue overflow-y-auto">
+      <div className="bg-velix-slate-blue overflow-y-auto h-screen">
         <div className="lg:hidden block fixed bottom-0 left-0 right-0 w-full bg-white z-40 py-7 border-t">
           <BottomBar />
         </div>
