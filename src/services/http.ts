031e9f5f53f461228cef2be9a23128390a1d10cc
Jonath-z <jonathanzihindula95@gmail.com>
2024-06-05 22:23:33 +0200

    refactor: replace db calls with velix api service

--- src/services/http.ts
+++ src/services/http.ts
@@ -1,10 +1,52 @@
 import { VELIX_API_URL } from "@/utils/constant";
-import axios from "axios";
+import { saveActionDto, saveStakerDto } from "@/utils/dto";
+import axios, { AxiosInstance } from "axios";
+
+export type Action = "mint" | "stake" | "unstake";
 
 const api = axios.create({
   baseURL: VELIX_API_URL
 });
 
+// ATTENTION: this function is not used and working anymore, should be remove whenever possible
 export async function claimFaucetToken(walletAddress: string) {
   return await api.post("/faucet", { receiver: walletAddress });
 }
+
+class VelixApi {
+  api: AxiosInstance;
+  constructor(api: AxiosInstance) {
+    this.api = api;
+  }
+
+  private actionEndpoint(action: Action) {
+    switch (action) {
+      case "mint":
+        return "/mints";
+      case "stake":
+        return "/stake";
+      case "unstake":
+        return "/unstakes";
+    }
+  }
+
+  async saveAction(action: Action, data: saveActionDto) {
+    return await this.api.put(this.actionEndpoint(action), data);
+  }
+
+  async retreiveActionsActivity(action: Action, walletAddress: string) {
+    return await this.api.get(
+      `${this.actionEndpoint(action)}/${walletAddress}`
+    );
+  }
+
+  async saveStaker(data: saveStakerDto) {
+    return await this.api.put("/stake/stakers", data);
+  }
+
+  async retreiveStakersNumber() {
+    return await this.api.get("/stake/stakers/count");
+  }
+}
+
+export const velixApi = new VelixApi(api);
---
+++ src/utils/dto.ts
@@ -0,0 +1,10 @@
+export type saveActionDto = {
+  walletAddress: string;
+  amount: number;
+  txHash: string;
+};
+
+export type saveStakerDto = {
+  walletAddress: string;
+  amount: number;
+};
