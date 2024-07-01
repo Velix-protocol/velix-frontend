ff01b9c140297f169ab203a347ee1afa67196512
Jonath-z <jonathanzihindula95@gmail.com>
2024-07-01 17:32:21 +0200

    style: upgrade the doc card style

--- src/components/ui/velix/cards/AuditCard.tsx
+++ src/components/ui/velix/cards/AuditCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaLongArrowAltRight } from "react-icons/fa";
+import GradientBorder from "../GradientBorder";
 
 interface CardProps {
   title: string;
@@ -9,20 +10,24 @@ interface CardProps {
 
 const AuditCard: React.FC<CardProps> = ({ title, iconSrc, link }) => {
   return (
-    <div className="w-full flex font-space-grotesk flex-col justify-between h-[360px] rounded overflow-hidden dark:border dark:bg-black dark:border-white/20 bg-velix-slate-blue m-4">
-      <div className="px-6 py-10 flex justify-center items-center h-4/6`">
-        <img
-          src={iconSrc}
-          alt={title}
-          width={iconSrc === "./book.svg" ? 220 : 140}
-          height={100}
-        />
-      </div>
-      <div className="flex justify-between px-6 py-4 bg-velix-primary text-white text-center rounded-b-lg ">
-        <span className="">{title}</span>
-        <a href={link} target="_blank">
-          <FaLongArrowAltRight className="hover:cursor-pointer" />
-        </a>
+    <div className="relative p-0.25">
+      <GradientBorder className="rounded-[0.9375rem] z-0 via-white/35" />
+      <div className="w-full flex font-space-grotesk flex-col justify-between h-[360px] dark:bg-gradient-to-r to-[#313131] from-[#1D1D1D] rounded-[0.9375rem] overflow-hidden relative bg-velix-slate-blue">
+        <div className="flex justify-center items-center h-4/6">
+          <img
+            src={iconSrc}
+            alt={title}
+            width={iconSrc === "./book.svg" ? 220 : 140}
+            height={100}
+            className="p-2 mt-4"
+          />
+        </div>
+        <div className="flex justify-between p-6 bg-velix-primary dark:bg-[#2E2E2E] text-white text-center rounded-b-lg ">
+          <span className="">{title}</span>
+          <a href={link} target="_blank">
+            <FaLongArrowAltRight className="hover:cursor-pointer" />
+          </a>
+        </div>
       </div>
     </div>
   );
