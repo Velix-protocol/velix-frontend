525f65a9038e277684b2e768d72abdf6ac0325bb
Jonath-z <jonathanzihindula95@gmail.com>
2024-07-03 09:24:49 +0200

    feat: improve wording

--- src/components/section/Mint.tsx
+++ src/components/section/Mint.tsx
@@ -221,7 +221,6 @@ export default function Mint() {
                       </span>
                       <span>
                         Mint veMETIS to participate in liquid staking.
-                        <br /> veMETIS is a liquid staking derivative of METIS.
                       </span>
                     </p>
                   }
