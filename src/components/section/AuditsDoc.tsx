8d2c44a17c439aec6e2a657146ebf9dd76275671
Jonath-z <jonathanzihindula95@gmail.com>
2024-07-10 19:18:14 +0200

    fix: update audit card icon paths in AuditsDoc component

--- src/components/section/AuditsDoc.tsx
+++ src/components/section/AuditsDoc.tsx
@@ -16,14 +16,14 @@ const AuditsDoc = () => {
         <div className="flex flex-col lg:grid grid-cols-2 py-16  justify-center gap-2 lg:gap-12 lg:px-1  items-center dark:bg-velix-primary bg-transparent dark:rounded-lg">
           <AuditCard
             title="Audit Report"
-            iconSrc="./audit.svg"
+            iconSrc="./svg/audit.svg"
             link={
               "https://github.com/peckshield/publications/blob/master/audit_reports/PeckShield-Audit-Report-Velix-v1.0.pdf"
             }
           />
           <AuditCard
             title="Documentation"
-            iconSrc="./book.svg"
+            iconSrc="./svg/book.svg"
             link={"https://docs.velix.io/"}
           />
         </div>
