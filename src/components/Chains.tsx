b52693ad4eeaffbf75888f27e068caa9cb47b456
Jonath-z <jonathanzihindula95@gmail.com>
2024-07-22 13:43:56 +0200

    fix(chains): add check for existing referral code display

--- src/components/Chains.tsx
+++ src/components/Chains.tsx
@@ -108,20 +108,25 @@ export default function Chains() {
 
   return (
     <div className="bg-white dark:bg-velix-form-dark-background rounded-xl flex flex-col gap-3 h-max p-5 lg:p-11">
-      <div className="bg-velix-slate-blue pr-7 font-space-grotesk dark:bg-velix-form-input-dark flex justify-between items-center p-2 rounded-lg">
-        <p className="bg-velix-blue/5 dark:text-velix-dark-white dark:bg-velix-light-dark px-3 py-2 flex items-center gap-3 font-space-grotesk rounded-lg">
-          Referral Code: {staker?.referralCode}
-        </p>
-        {staker?.referralCode.toLowerCase() === copiedAddress.toLowerCase() ? (
-          <Check className="text-velix-primary w-5 h-5 dark:text-velix-icon-dark" />
-        ) : (
-          <Link
-            role="button"
-            onClick={() => onCopyToClickboard(staker?.referralCode ?? "", true)}
-            className="text-velix-primary w-5 h-5 cursor-pointer dark:text-velix-icon-dark"
-          />
-        )}
-      </div>
+      {staker?.referralCode && (
+        <div className="bg-velix-slate-blue pr-7 font-space-grotesk dark:bg-velix-form-input-dark flex justify-between items-center p-2 rounded-lg">
+          <p className="bg-velix-blue/5 dark:text-velix-dark-white dark:bg-velix-light-dark px-3 py-2 flex items-center gap-3 font-space-grotesk rounded-lg">
+            Referral Code: {staker?.referralCode}
+          </p>
+          {staker?.referralCode.toLowerCase() ===
+          copiedAddress.toLowerCase() ? (
+            <Check className="text-velix-primary w-5 h-5 dark:text-velix-icon-dark" />
+          ) : (
+            <Link
+              role="button"
+              onClick={() =>
+                onCopyToClickboard(staker?.referralCode ?? "", true)
+              }
+              className="text-velix-primary w-5 h-5 cursor-pointer dark:text-velix-icon-dark"
+            />
+          )}
+        </div>
+      )}
       {avalableChains.map((chain) => {
         return (
           <div
