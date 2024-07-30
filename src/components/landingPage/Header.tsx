9cc63966db3ad1e850c4db6453e9e8540afa679d
Joe Masu <josephmuhindo089@gmail.com>
2024-07-31 01:12:29 +0300

    feat: add toggle menu one mobile devices

--- src/components/landingPage/Header.tsx
+++ src/components/landingPage/Header.tsx
@@ -1,12 +1,12 @@
 import VelixLogo from "@/components/svg/VelixLogoGroup";
+import VelixBlackLogo from "@/components/svg/VelixPrimaryLogoBlack";
 import { useInView } from "react-intersection-observer";
 import { Link, useNavigate } from "react-router-dom";
 import { Button } from "../ui/button";
 import ThemeButton from "../ui/velix/ThemeButton";
-
-import { HiOutlineX } from 'react-icons/hi';
-import { HiChevronDown, HiMenu } from "react-icons/hi";
+import { HiOutlineX, HiChevronDown, HiMenu } from "react-icons/hi";
 import { useState } from "react";
+import { useTheme } from "@/context/theme-provider";
 
 export default function Header() {
   const { ref, inView } = useInView({
@@ -15,8 +15,21 @@ export default function Header() {
   });
 
   const navigate = useNavigate();
+  const { theme } = useTheme();
   const [isDropdownOpened, setIsDropDownOpened] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const links = [
+    { to: "https://docs.velix.io", text: "Docs" },
+    { to: "https://github.com/peckshield/publications/blob/master/audit_reports/PeckShield-Audit-Report-Velix-v1.0.pdf", text: "Audits" },
+    { to: "/app/dashboard", text: "VePoints" }
+  ];
+
+  const drawerStyles = `fixed top-0 left-0 h-full w-full ${
+    theme === "light" ? "bg-white text-black" : "bg-velix-primary text-white"
+  } transition-transform duration-300 ease-in-out ${
+    isMenuOpen ? "translate-x-0" : "-translate-x-full"
+  } z-50 flex flex-col justify-between`;
 
   return (
     <div>
@@ -25,7 +38,11 @@ export default function Header() {
         className="flex justify-between items-center py-5 lg:py-14 z-50 relative"
       >
         <Link to="/">
-          <VelixLogo className="w-[4.25rem] h-4 lg:h-5 lg:w-[5rem]" />
+          {theme === "light" ? (
+            <VelixBlackLogo className="w-[5.625rem] h-[1.4375rem]" />
+          ) : (
+            <VelixLogo className="w-[5.625rem] h-[1.4375rem]" />
+          )}
         </Link>
         <div className="flex items-center gap-3">
           <div className="relative px-2 hidden lg:flex">
@@ -33,28 +50,16 @@ export default function Header() {
               data-dropdownopened={isDropdownOpened}
               className="flex items-center gap-4 pb-2"
             >
-              <Link
-                to="https://docs.velix.io"
-                target="_blank"
-                className="font-space-grotesk !font-normal text-white flex items-center hover:underline gap-1 relative after:absolute "
-              >
-                Docs
-              </Link>
-              <Link
-                to="https://github.com/peckshield/publications/blob/master/audit_reports/PeckShield-Audit-Report-Velix-v1.0.pdf"
-                target="_blank"
-                className="font-space-grotesk !font-normal text-white flex items-center hover:underline gap-1 relative after:absolute "
-              >
-                Audits
-              </Link>
-              <Link
-                to="/app/dashboard"
-                target="_blank"
-                className="font-space-grotesk !font-normal text-white flex items-center hover:underline gap-1 relative after:absolute "
-              >
-                VePoints
-              </Link>
-
+              {links.map((link) => (
+                <Link
+                  key={link.to}
+                  to={link.to}
+                  target="_blank"
+                  className="font-space-grotesk !font-normal text-white flex items-center hover:underline gap-1 relative after:absolute"
+                >
+                  {link.text}
+                </Link>
+              ))}
             </div>
 
             {isDropdownOpened && (
@@ -73,7 +78,6 @@ export default function Header() {
           >
             Launch
           </Button>
-
           <button
             className="lg:hidden p-2"
             onClick={() => setIsMenuOpen(true)}
@@ -83,46 +87,35 @@ export default function Header() {
         </div>
       </header>
 
-      <div
-        className={`fixed top-0 left-0 h-full w-full bg-velix-primary text-white transition-transform duration-300 ease-in-out ${
-          isMenuOpen ? "translate-x-0" : "-translate-x-full"
-        } z-50 flex flex-col justify-between`}
-      >
+      <div className={drawerStyles}>
         <div className="flex justify-between items-center p-4">
           <Link to="/">
-            <VelixLogo className="w-[4.25rem] h-4 lg:h-5 lg:w-[5rem]" />
+            {theme === "light" ? (
+              <VelixBlackLogo className="w-[4.25rem] h-4 lg:h-5 lg:w-[5rem] text-black" />
+            ) : (
+              <VelixLogo className="w-[4.25rem] h-4 lg:h-5 lg:w-[5rem] text-white" />
+            )}
           </Link>
           <button
-            className="text-white"
+            className="text-current"
             onClick={() => setIsMenuOpen(false)}
           >
             <HiOutlineX className="w-6 h-6 transform rotate-180" />
           </button>
         </div>
         <div className="flex flex-col p-4 space-y-4 flex-grow">
-          <Link
-            to="/app/dashboard"
-            target="_blank"
-            className="font-space-grotesk !font-normal text-white relative after:absolute after:bottom-[-5px] after:left-0 after:w-full after:h-[1px] after:bg-neutral-800"
-          >
-            Vepoints
-          </Link>
-          <Link
-            to="https://docs.velix.io"
-            target="_blank"
-            className="font-space-grotesk !font-normal text-white relative after:absolute after:bottom-[-5px] after:left-0 after:w-full after:h-[1px] after:bg-neutral-800"
-          >
-            Docs
-          </Link>
-          <Link
-            to="https://github.com/peckshield/publications/blob/master/audit_reports/PeckShield-Audit-Report-Velix-v1.0.pdf"
-            target="_blank"
-            className="font-space-grotesk !font-normal text-white relative "
-          >
-            Audit
-          </Link>
+          {links.map((link) => (
+            <Link
+              key={link.to}
+              to={link.to}
+              target="_blank"
+              className="font-space-grotesk !font-normal relative after:absolute after:bottom-[-5px] after:left-0 after:w-full after:h-[1px] after:bg-neutral-800"
+            >
+              {link.text}
+            </Link>
+          ))}
         </div>
-        <div className="p-4 flex flex-row justify-center text-white">
+        <div className="p-4 flex flex-row justify-center">
           <ThemeButton className="items-center" />
         </div>
       </div>
@@ -134,10 +127,14 @@ export default function Header() {
       >
         <div className="flex justify-between items-center w-full max-w-5xl xl:max-w-7xl mx-auto relative">
           <Link to="/">
-            <VelixLogo className="w-[4.25rem] h-4 lg:h-5 lg:w-[5rem]" />
+            {theme === "light" ? (
+              <VelixBlackLogo className="w-[4.25rem] h-4 lg:h-5 lg:w-[5rem] text-black" />
+            ) : (
+              <VelixLogo className="w-[4.25rem] h-4 lg:h-5 lg:w-[5rem] text-white" />
+            )}
           </Link>
           <div className="flex items-center gap-3">
-            <div className="relative px-2">
+            <div className="relative px-2 hidden lg:flex">
               <div
                 data-dropdownopened={isDropdownOpened}
                 className="flex items-center gap-4 pb-2"
@@ -145,7 +142,7 @@ export default function Header() {
                 <Link
                   to="https://docs.velix.io"
                   target="_blank"
-                  className="hidden lg:block font-space-grotesk !font-normal text-white flex items-center underline gap-1 relative after:absolute"
+                  className="hidden lg:block font-space-grotesk !font-normal text-white items-center underline gap-1 relative after:absolute"
                 >
                   Docs
                 </Link>
