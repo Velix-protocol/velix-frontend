15800312df606f9e97697190c94267222feb48bf
@Jonath-z <jonathanzihindula95@gmail.com>
2024-08-07 16:57:12 +0200

    feat: add vePointpage (#95)
    * feat: add vePointpage
    
    * feat: add redeem logic
    
    * feat: comment redeem point on dashboard and add darkmode

--- src/components/layouts/Page.tsx
+++ src/components/layouts/Page.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import AppFooter from "../app/AppFooter";
 import AppHeader from "../app/AppHeader";
-import BottomBar from "../app/BottomBar";
+import NavigationMenus from "../app/NavigationMenus";
 import WagmiProvider from "@/context/WagmiProvider";
 
 export default function Page() {
@@ -9,7 +9,7 @@ export default function Page() {
     <WagmiProvider>
       <div className="bg-velix-slate-blue dark:bg-velix-primary flex flex-col justify-between gap-3 overflow-y-auto h-screen">
         <div className="lg:hidden block fixed bottom-0 left-0 right-0 w-full bg-white dark:bg-velix-page-dark z-40 py-7 border-t">
-          <BottomBar />
+          <NavigationMenus />
         </div>
         <AppHeader />
         <Outlet />
