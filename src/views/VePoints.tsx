dc881a3f3ac55ba114f20f98b7d5eea034cb266c
Jonath-z <jonathanzihindula95@gmail.com>
2024-08-15 13:58:00 +0200

    style: improve the styles

--- src/views/VePoints.tsx
+++ src/views/VePoints.tsx
@@ -22,11 +22,11 @@ function VePointDescriptionSection({
   children: ReactNode;
 }) {
   return (
-    <div className="font-space-grotesk bg-white dark:bg-velix-form-dark-background rounded-2xl">
+    <div className="font-space-grotesk">
       <h4 className="bg-velix-primary dark:bg-velix-light-dark py-10 px-11 max-sm:px-5 max-lg:text-xl max-lg:py-5 text-3xl rounded-t-2xl text-white font-bold">
         {title}
       </h4>
-      <div className="px-11 max-lg:px-5 max-lg:py-5 text-velix-gray space-y-4 py-10 rounded-b-2xl max-w-[90%]">
+      <div className="px-11 max-lg:px-5 max-lg:py-5 text-velix-gray space-y-4 py-10 rounded-2xl -mt-3 z-10 w-full bg-white dark:bg-velix-form-dark-background">
         {children}
       </div>
     </div>
@@ -131,8 +131,8 @@ export default function VePoints() {
             </VePointDescriptionSection>
             <VePointDescriptionSection title="Referral points">
               <p>
-                <b>Base Referral Points:</b> Points earned for each successful
-                referral.
+                <b>Base Referral Points:</b> Points earned for each <br />{" "}
+                successful referral.
               </p>
               <p>
                 <b>Referral Bonus: </b> Additional points if the referred user
