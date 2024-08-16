8b5f96cb2ccc96d44cbe2dae74cf7cada99d4ffb
Jonath-z <jonathanzihindula95@gmail.com>
2024-08-16 12:35:23 +0200

    refactor: clean up the header on the landing page

---
+++ src/components/landingPage/Header/HeaderMini.tsx
@@ -0,0 +1,33 @@
+import { Dispatch, SetStateAction } from "react";
+import Navigation, { HeaderLink } from "./Navigation";
+
+type HeaderMiniProps = {
+  inView: boolean;
+  links: HeaderLink;
+  isMenuOpen: boolean;
+  setIsMenuOpen: Dispatch<SetStateAction<boolean>>;
+  navigateToApp: () => void;
+};
+
+export default function HeaderMini({
+  inView,
+  links,
+  isMenuOpen,
+  setIsMenuOpen,
+  navigateToApp
+}: HeaderMiniProps) {
+  return (
+    <header
+      className={`justify-between -top-full -translate-y-full fixed left-0 right-0 z-[999] items-center bg-velix-primary ${
+        !inView ? "!top-0 !translate-y-0 transition-all duration-200" : ""
+      }`}
+    >
+      <Navigation
+        isMenuOpen={isMenuOpen}
+        setIsMenuOpen={setIsMenuOpen}
+        navigateToApp={navigateToApp}
+        links={links}
+      />
+    </header>
+  );
+}
---
+++ src/components/landingPage/Header/SideMenu.tsx
@@ -0,0 +1,43 @@
+import { HeaderLink } from "./Navigation";
+import ThemeButton from "@/components/ui/velix/ThemeButton";
+import { Link } from "react-router-dom";
+
+type SideMenuProps = {
+  links: HeaderLink;
+  isMenuOpen: boolean;
+};
+
+export default function SideMenu({ isMenuOpen, links }: SideMenuProps) {
+  return (
+    <div
+      data-menuopened={isMenuOpen}
+      className="fixed lg:hidden top-16 right-0 bottom-0 h-screen w-full transition-transform duration-300 z-50 flex flex-col ease-in-out bg-white text-black dark:bg-velix-primary dark:text-white data-[menuopened=true]:translate-x-0 data-[menuopened=false]:translate-x-full"
+    >
+      <div className="flex flex-col flex-grow overflow-hidden scrollbar-hidden">
+        {links.map((link) => (
+          <Link
+            key={link.to}
+            to={link.to}
+            target="_blank"
+            className={`font-space-grotesk !font-normal relative p-7 after:absolute after:bottom-[-2px] after:left-[-1px] after:w-[calc(100%+2px)] after:h-[1px] ${
+              link.text === "VePoints"
+                ? "after:bg-transparent"
+                : "after:bg-neutral-400 after:opacity-30 dark:after:bg-neutral-600"
+            }`}
+          >
+            {link.text}
+          </Link>
+        ))}
+        <ThemeButton className="w-12 h-12 flex items-center justify-center p-3 ml-4" />
+      </div>
+      <div className="flex flex-col justify-end flex-grow mb-20">
+        <div className="font-space-grotesk !font-normal relative text-neutral-600 dark:text-neutral-300">
+          <div className="absolute top-[-3.5rem] left-0 w-full h-[1px] bg-neutral-700 opacity-30 dark:bg-neutral-600 transition-opacity duration-300" />
+          <div className="p-6 mt-[-3rem]">
+            © 2023 - {new Date().getFullYear()} Velix
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
