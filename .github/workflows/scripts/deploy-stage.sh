542965019bed350e31afcfb145decde6aa76bda0
Jonath-z <jonathanzihindula95@gmail.com>
2024-09-05 00:37:44 +0200

    chore: add deployment workflow

---
+++ .github/workflows/scripts/deploy-stage.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e  # Exit immediately if a command exits with a non-zero status.
+
+echo "Starting deployment process..."
+
+# Install dependencies
+echo "Installing dependencies..."
+pnpm install
+
+# Build the application
+echo "Building the application..."
+pnpm run build:stage
+
+# Moving the built app to linux web location
+echo "Moving the build to the www"
+mv dist/* /var/www/velix-frontend
+
+echo "Deployment completed successfully!"
