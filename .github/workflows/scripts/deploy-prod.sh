1d93d9b2c71e3e200f74882fcdd1d02887a321f6
Jonath-z <jonathanzihindula95@gmail.com>
2024-09-05 18:54:26 +0200

    feat: add deployment workflow

---
+++ .github/workflows/scripts/deploy-prod.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e  # Exit immediately if a command exits with a non-zero status.
+
+echo "Starting deployment process..."
+
+# Install dependencies
+echo "Installing dependencies..."
+pnpm install
+
+# Build the application
+echo "Building the application..."
+pnpm run build
+
+# Moving the built app to linux web location
+echo "Moving the build to the www"
+rm -rf /var/www/velix-frontend/*
+mv dist/* /var/www/velix-frontend/
+
+echo "Deployment completed successfully!"
