610abd8023a428e977e20c1d86ba1b70a30f9d87
josephMasu <josephmuhindo089@gmail.com>
2025-01-06 13:23:49 +0300

    refactor: Refactor StarknetCardCard changing p-5 to p-8

--- src/components/ui/velix/cards/StarknetCard.tsx
+++ src/components/ui/velix/cards/StarknetCard.tsx
@@ -2,8 +2,8 @@ import { Button } from "../../button";
 
 export default function StarknetCard(){
     return(
-        <div className="rounded-lg bg-white p-5 mt-12 w-full dark:bg-velix-claim-gray">
-            <div className="flex bg-velix-claim dark:bg-velix-claim-gray2 p-3 rounded-lg flex-col ">
+        <div className="rounded-lg bg-white p-8 mt-12 w-full dark:bg-velix-claim-gray">
+            <div className="flex bg-velix-claim dark:bg-velix-claim-gray2 px-5 py-3 rounded-lg flex-col ">
                 <div className="flex-1 ">
                     <p className="text-velix-claim-grey dark:text-velix-claim text-sm lg:text-base font-space-grotesk">
                     veSTRK balance
@@ -11,7 +11,8 @@ export default function StarknetCard(){
                     <div className="items-start xl:items-center mt-1">
                         <div className="flex md:flex-row items-center text-velix-claim-gray text-sm lg:text-base font-space-grotesk  dark:text-white">
                             <span className="text-velix-blue dark:text-white font-bold font-space-grotesk">
-                            00.0 veSTRK                            </span>
+                            00.0 veSTRK                            
+                            </span>
                         </div>
                     </div>
                 </div>
