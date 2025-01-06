26c49aafce1b2486f38687514c89f0c55a92e32a
Joseph Masu <60499662+JosephMasu@users.noreply.github.com>
2025-01-06 19:13:17 +0300

    feat: create Crosschain page and add routing for /app/crosschain (#150)
    * feat: create Crosschain page and add routing for /app/crosschain
    
    * refactor: changing Claim to Send on the InputWalletAddress
    
    * refactor: replace arrow function with function declaration
    
    * refactor: adding the text to the AddWalletCard
    
    * refactor: Refactor MetisCard changing p-5 to p-8
    
    * refactor: Refactor StarknetCardCard changing p-5 to p-8
    
    * refactor: Refactor InputWalletAddress changing p-5 to p-8

---
+++ src/components/app/Crosschain/InputWalletAddress.tsx
@@ -0,0 +1,43 @@
+import { Button } from "@/components/ui/button";
+import React, { useState, ChangeEvent } from "react";
+
+interface InputWalletAddressProps {
+  onClaim: () => void;
+}
+
+const InputWalletAddress: React.FC<InputWalletAddressProps> = ({ onClaim }) => {
+  const [walletAddress, setWalletAddress] = useState<string>("");
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    setWalletAddress(event.target.value);
+  };
+
+  return (
+    <div className="flex flex-col rounded-lg bg-white p-8 mt-12 w-full dark:bg-velix-claim-gray">
+      <div className="flex bg-white dark:bg-velix-claim-gray2 rounded-lg flex-col sm:flex-row">
+        <div className="flex-1">
+          <div className="items-start xl:items-center mt-1">
+            <input
+              type="text"
+              className="w-full h-11 p-2 bg-velix-claim dark:bg-velix-claim-gray2 rounded-md text-sm text-white focus:outline-none"
+              placeholder="Paste your wallet address"
+              value={walletAddress}
+              onChange={handleInputChange}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="mt-4 w-full">
+        <Button
+          onClick={onClaim} 
+          className="lg:w-full w-full font-space-grotesk bg-velix-blue dark:bg-velix-gray text-white dark:text-velix-claim-gray px-10"
+          disabled={!walletAddress} 
+        >
+          Send
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default InputWalletAddress;
---
+++ src/views/Crosschain.tsx
@@ -0,0 +1,13 @@
+import Section from "@/components/layouts/Section";
+import LineChart from "@/components/app/Crosschain/LineChart";
+import ClaimOnCrosschain from "@/components/app/Crosschain/ClaimOnCrosschain";
+// import AddWalletCard from "@/components/ui/velix/cards/AddWalletCard";
+export default function Crosschain(){
+    return(
+        <Section className="max-lg:mx-5 py-32" >
+            <LineChart/>
+            {/* <AddWalletCard/> */}
+            <ClaimOnCrosschain/>
+        </Section>
+    )
+}
