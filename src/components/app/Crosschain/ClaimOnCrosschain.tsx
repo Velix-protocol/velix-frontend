d65fbaaf646a7594a08d225b8ff97986ff9d950b
Jonath-z <jonathanzihindula95@gmail.com>
2025-01-18 12:12:44 +0200

    refactor: format file and use cn for class merge

--- src/components/app/Crosschain/ClaimOnCrosschain.tsx
+++ src/components/app/Crosschain/ClaimOnCrosschain.tsx
@@ -5,7 +5,7 @@ import MetisCard from "@/components/ui/velix/cards/MetisCard";
 import StarknetCard from "@/components/ui/velix/cards/StarknetCard";
 import InputWalletAddress from "./InputWalletAddress";
 import { useState } from "react";
-export default function ClaimOnCrosschain(){
+export default function ClaimOnCrosschain() {
   const [walletSubmitted, setWalletSubmitted] = useState<boolean>(false);
 
   const handleClaim = (): void => {
@@ -13,9 +13,10 @@ export default function ClaimOnCrosschain(){
   };
 
   return (
-      <Section className="pb-32 lg:pb-16">
-        {/* <StakeTitleWrapper className="pt-20">
-          <div className="w-full">
+    <Section className="pb-32 lg:pb-16">
+      <AppContent>
+        <div className="w-full h-fit">
+          <div className="w-full block lg:mt-20 mt-8">
             {!walletSubmitted ? (
               <Title
                 name="Paste your wallet address"
@@ -28,50 +29,25 @@ export default function ClaimOnCrosschain(){
               />
             )}
           </div>
-          <div className="w-full">
-          {!walletSubmitted && (
-            <Title
-                name="Claim crosschain rewards"
-                subtitle="Claim METIS rewards 24hrs after submitting wallet"
-            />
-            )}
-          </div>
-        </StakeTitleWrapper> */}
-        <AppContent>
-          <div className="w-full h-fit">
-            <div className="w-full block  lg:mt-20 mt-8">
-            {!walletSubmitted ? (
-              <Title
-                name="Paste your wallet address"
-                subtitle="Paste your ethereum wallet address."
-              />
-            ) : (
-              <Title
-                name="Claim crosschain rewards"
-                subtitle="Claim METIS rewards 24hrs after submitting wallet."
-              />
-            )}
-            </div>
-            {!walletSubmitted ? (
-              <InputWalletAddress onClaim={handleClaim} />
-            ) : (
-              <MetisCard />
-            )}
-          </div>
-          <div className="w-full">
-            <div className="w-full block lg:mt-20 mt-8">
+          {!walletSubmitted ? (
+            <InputWalletAddress onClaim={handleClaim} />
+          ) : (
+            <MetisCard />
+          )}
+        </div>
+        <div className="w-full">
+          <div className="w-full block lg:mt-20 mt-8">
             {!walletSubmitted && (
-            <Title
+              <Title
                 name="Claim crosschain rewards"
                 subtitle="Claim METIS rewards 24hrs after submitting wallet"
-            />
+              />
             )}
-            </div>
-            {!walletSubmitted && <MetisCard />}
-            <StarknetCard className={walletSubmitted ? 'mt-[13rem]' : ''} />
-            </div>
-        </AppContent>
-      </Section>
+          </div>
+          {!walletSubmitted && <MetisCard />}
+          <StarknetCard className={walletSubmitted ? "mt-[13rem]" : ""} />
+        </div>
+      </AppContent>
+    </Section>
   );
 }
-
--- src/components/ui/velix/cards/StarknetCard.tsx
+++ src/components/ui/velix/cards/StarknetCard.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/utils/utils";
 import { Button } from "../../button";
 
 interface StarknetCardProps {
@@ -5,27 +6,32 @@ interface StarknetCardProps {
 }
 
 export default function StarknetCard({ className = "" }: StarknetCardProps) {
-    return (
-        <div className={`rounded-lg bg-white p-8 mt-12 w-full dark:bg-velix-claim-gray ${className}`}>
-            <div className="flex bg-velix-claim dark:bg-velix-claim-gray2 px-5 py-3 rounded-lg flex-col ">
-                <div className="flex-1 ">
-                    <p className="text-velix-claim-grey dark:text-velix-claim text-sm lg:text-base font-space-grotesk">
-                        veSTRK balance
-                    </p>
-                    <div className="items-start xl:items-center mt-1">
-                        <div className="flex md:flex-row items-center text-velix-claim-gray text-sm lg:text-base font-space-grotesk dark:text-white">
-                            <span className="text-velix-blue dark:text-white font-bold font-space-grotesk">
-                                00.0 veSTRK
-                            </span>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div
+      className={cn(
+        "rounded-lg bg-white p-8 mt-12 w-full dark:bg-velix-claim-gray",
+        className
+      )}
+    >
+      <div className="flex bg-velix-claim dark:bg-velix-claim-gray2 px-5 py-3 rounded-lg flex-col ">
+        <div className="flex-1 ">
+          <p className="text-velix-claim-grey dark:text-velix-claim text-sm lg:text-base font-space-grotesk">
+            veSTRK balance
+          </p>
+          <div className="items-start xl:items-center mt-1">
+            <div className="flex md:flex-row items-center text-velix-claim-gray text-sm lg:text-base font-space-grotesk dark:text-white">
+              <span className="text-velix-blue dark:text-white font-bold font-space-grotesk">
+                00.0 veSTRK
+              </span>
             </div>
-            <div className="mt-4 w-full">
-                <Button className="lg:w-full w-full font-space-grotesk bg-velix-blue dark:bg-velix-gray text-white dark:text-velix-claim-gray px-10">
-                    Claim
-                </Button>
-            </div>
+          </div>
         </div>
-    );
+      </div>
+      <div className="mt-4 w-full">
+        <Button className="lg:w-full w-full font-space-grotesk bg-velix-blue dark:bg-velix-gray text-white dark:text-velix-claim-gray px-10">
+          Claim
+        </Button>
+      </div>
+    </div>
+  );
 }
