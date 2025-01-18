309decda6e0ff12696a5c74642d49bfe853d03d4
josephMasu <josephmuhindo089@gmail.com>
2025-01-18 03:25:39 +0300

    refactor: Adding the WalletCard to the navigation menues: stake, redeem and vepoints

--- src/components/app/Stake/index.tsx
+++ src/components/app/Stake/index.tsx
@@ -4,12 +4,14 @@ import StakingOperations from "./StakingOperations";
 import AppContent from "@/components/layouts/AppContent";
 import Statitics from "../Statitics";
 import StakeTitleWrapper from "@/components/layouts/StakeTitleWrapper";
+import AddWalletCard from "@/components/ui/velix/cards/AddWalletCard";
 
 export default function Stake() {
   return (
     <div>
-      <Section className="px-5 pb-32 lg:pb-16">
-        <StakeTitleWrapper>
+      <Section className="px-5 pb-32 lg:pb-16 mt-5">
+        <AddWalletCard/>
+        <StakeTitleWrapper className="lg:pt-20">
           <div className="w-full">
             <Title
               name="Stake METIS"
@@ -25,7 +27,7 @@ export default function Stake() {
         </StakeTitleWrapper>
         <AppContent>
           <div className="w-full h-fit">
-            <div className="w-full block lg:hidden mt-32">
+            <div className="w-full block lg:hidden mt-10">
               <Title
                 name="Stake METIS"
                 subtitle="Stake METIS and receive veMETIS"
--- src/components/ui/velix/cards/AddWalletCard.tsx
+++ src/components/ui/velix/cards/AddWalletCard.tsx
@@ -10,17 +10,19 @@ export default function AddWalletCard() {
   };
 
   return (
-    <div className="flex border border-velix-claim-red p-3 rounded-lg flex-row w-full h-fit">
-      <AddWallet className="items-center mt-2 w-6 h-6 sm:w-5 sm:h-5 flex-shrink-0" />
-      <p className="ml-2 text-velix-claim-red text-xs lg:text-base font-space-grotesk lg:mt-1 mt-2 sm:w-3/4 truncate">
-        Add your wallet address for Genesis pool stakes share a 20% protocol revenue for Velix.
-      </p>      
-      <Button 
-        onClick={handleAddWalletClick} 
-        className="w-auto h-auto font-space-grotesk bg-velix-claim-red text-white ml-auto py-1"
-      >
-        Add Wallet
-      </Button>
+    <div className="pt-32">
+      <div className="border border-velix-claim-red p-3 rounded-lg w-full h-fit flex items-center">         
+        <AddWallet className="w-6 h-6 sm:w-5 sm:h-5 flex-shrink-0 ml-2" /> 
+        <p className="text-velix-claim-red text-xs -mt-1 lg:text-base font-space-grotesk sm:w-3/4 flex-grow-1 px-2"> 
+          Add your wallet address for Genesis pool stakes share a 20% protocol revenue for Velix.
+        </p>      
+        
+        <Button 
+          onClick={handleAddWalletClick} 
+          className="font-space-grotesk bg-velix-claim-red text-white lg:px-4 px-1 lg:text-base text-xs ml-auto sm:text-xs">
+          Add Wallet
+        </Button>
+      </div>
     </div>
   );
 }
