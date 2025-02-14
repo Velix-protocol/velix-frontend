feebd9edd577a0cadd381c42807e0b6029acc246
Jonath-z <jonathanzihindula95@gmail.com>
2025-02-14 11:43:03 +0200

    fix: comment chart component

--- src/views/Crosschain.tsx
+++ src/views/Crosschain.tsx
@@ -1,10 +1,10 @@
 import Section from "@/components/layouts/Section";
-import LineChart from "@/components/app/Crosschain/LineChart";
+// import LineChart from "@/components/app/Crosschain/LineChart";
 import ClaimOnCrosschain from "@/components/app/Crosschain/ClaimOnCrosschain";
 export default function Crosschain() {
   return (
     <Section className="max-lg:mx-5 pt-32 ">
-      <LineChart />
+      {/* <LineChart /> */}
       <ClaimOnCrosschain />
     </Section>
   );
